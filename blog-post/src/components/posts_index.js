0a7058ceaf1622296abbafc17cf0bfd3584ca173
olzhas2102 <olzhas2102@yahoo.com>
2018-04-12 22:34:01 +0200

    fetching data with lifecycle method

--- blog-post/src/components/posts_index.js
+++ blog-post/src/components/posts_index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default () => {
-  return <div>List of blog posts</div>
-};
+class PostsIndex extends React.Component {
+  componentWillMount() {
+    console.log('Good time to call action creator to fetch posts');
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+export default PostsIndex;

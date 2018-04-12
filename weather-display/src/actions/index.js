2c44daa212ed74315b3e3f37d57f0e748145e7b4
olzhas2102 <olzhas2102@yahoo.com>
2018-04-12 10:13:20 +0200

    sparklines charts

--- weather-display/src/actions/index.js
+++ weather-display/src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY ='99dbe65f9b8c7fb33a5eb3cb94684501';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},de`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
--- weather-display/src/containers/search_bar.js
+++ weather-display/src/containers/search_bar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends React.Component {
+class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
 
     this.state = { term: '' };
   }
@@ -15,6 +19,9 @@ export default class SearchBar extends React.Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -33,3 +40,9 @@ export default class SearchBar extends React.Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);

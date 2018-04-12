afd1036bae20a3cd95221dd2255935897961cf99
olzhas2102 <olzhas2102@yahoo.com>
2018-04-12 21:44:27 +0200

    nesting of routes

--- blog-post/src/routes.js
+++ blog-post/src/routes.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 
+const Greeting = () => {
+  return <div>Hi there!</div>;
+};
+
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <Route path="greet" component={Greeting} />
+  </Route>
 );

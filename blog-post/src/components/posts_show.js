d64462261953c1d382144352b05e5ab3f96eea34
olzhas2102 <olzhas2102@yahoo.com>
2018-04-13 10:00:39 +0200

    loading data on render

--- blog-post/src/components/posts_show.js
+++ blog-post/src/components/posts_show.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends React.Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render() {
     return <div>Show post {this.props.params.id}</div>;
   }
 }
 
-export default PostsShow;
+export default connect(null, { fetchPost })(PostsShow);

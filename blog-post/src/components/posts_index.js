78ba0aa91b6ca8e0447c9e73c4e46c3ac64c02a3
olzhas2102 <olzhas2102@yahoo.com>
2018-04-13 09:37:32 +0200

    posts index

--- blog-post/src/components/posts_index.js
+++ blog-post/src/components/posts_index.js
@@ -8,22 +8,40 @@ class PostsIndex extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map(post => {
+      return (
+        <li key={post.id} className="list-group-item">
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <div>List of blog posts</div> 
         <div className="text-xs-right">
           <Link to="/posts/new" className="btn btn-warning">
             Add a Post
           </Link>
         </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
 }
 
 //function mapDispatchToProps(dispatch) {
 //  return bindActionCreators({ fetchPosts }, dispatch);
 //}
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
